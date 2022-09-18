36feea28cfef37d2c7dbe6d1ddd4b1c29c9e58d6
AqibChattha <aqibali543@gmail.com>
2022-09-19 02:01:02 +0500

    changes (1)

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Ernest Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Ernest Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Ernest Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Ernest Achiever",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <div className="container testimonials__container">
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
